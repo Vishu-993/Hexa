8433572dd7db09ee48949e245e18df3500e1689d
V-I-S-H-A-L <116909259+Vishu-993@users.noreply.github.com>
2022-12-22 19:49:13 +0530

    Update _pm2.js

--- commands/_pm2.js
+++ commands/_pm2.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Astro-Md
+ * @project_name : Hexa-Md
  * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Astro,A Multi-functional whatsapp bot.
+ * @description : Hexa,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
